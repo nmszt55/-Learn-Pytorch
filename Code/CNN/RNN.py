2f60f8e4143ca3652de445a54ca65ed7bdce66e7
Zhaozijian <zhaozijianxxx@163.com>
2021-11-20 20:50:22 +0800

    修改RNN代码问题

--- Code/CNN/RNN.py
+++ Code/CNN/RNN.py
@@ -16,10 +16,10 @@ class RNNModel(nn.Module):
         self.dense = nn.Linear(self.hidden_size, vocab_size)
         self.state = None
 
-    def forward(self, inputs, states):
+    def forward(self, inputs, state):
         # 获取one-hot向量表示
         X = to_onehot(inputs, self.vocab_size)
-        Y, self.state = self.rnn(torch.stack(X), self.state)
+        Y, self.state = self.rnn(torch.stack(X), state)
         # 全连接层首先会将Y的形状变为(num_steps * batch_size, num_hiddens),它的输出
         # 形状为(num_steps * batch_size, vocab_size)
         output = self.dense(Y.view(-1, Y.shape[-1]))
--- Code/Example/RNN_JayLyric_Simple.py
+++ Code/Example/RNN_JayLyric_Simple.py
@@ -20,16 +20,23 @@ index_list, char2idx, index2char, vocab_size = load_data_jay_lyrics()
 
 
 # -- 定义模型 --
+num_epoch = 250
+
 num_hiddens = 256
 rnn_layer = nn.RNN(input_size=vocab_size, hidden_size=num_hiddens)
 
 num_steps = 35
-batch_size = 2
+batch_size = 32
+lr = 1e-3
+clipping_theta = 1e2
 state = None
 X = torch.rand(num_steps, batch_size, vocab_size)
 
+predict_period = 50
+predict_len = 50
 
 if __name__ == '__main__':
+    print("using %s" % device)
     rnn = RNNModel(rnn_layer, vocab_size)
-    train_and_predict_rnn_torch(rnn, num_hiddens, vocab_size, device, index_list, index2char, char2idx, 250,
-                                num_steps, 0.001, 0.01, batch_size, 50, 50, ["分开", "不分开"])
+    train_and_predict_rnn_torch(rnn, num_hiddens, vocab_size, device, index_list, index2char, char2idx, num_epoch,
+                                num_steps, lr, clipping_theta, batch_size, predict_period, predict_len, ["爱你"])
